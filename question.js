a3e0eb4e1c8508ebad83154d42c55f3f576b1604
allthingscas <48394426+allthingscas@users.noreply.github.com>
2019-03-11 11:51:19 -0500

    Create question.js

---
+++ question.js
@@ -0,0 +1,14 @@
+function Question(text) {
+  this.text = " " + text + " ";
+  this.answer = " ";
+  this.answered = false;
+ }
+ 
+ Question.prototype.Answer = function(choice){
+  return this.answer;
+ }
+ 
+ Question.prototype.storeAnswer = function(x){
+  this.answer = x;
+  return;
+ }
