18356e82dd8233b37a434da955ba52dbb7959981
allthingscas <48394426+allthingscas@users.noreply.github.com>
2019-03-26 11:05:31 -0500

    Update app.js

--- Proposal/app.js
+++ Proposal/app.js
@@ -1,15 +1,15 @@
 var Proposalquestions = [
-"1. Give your CAS activity a descriptive title and date.",
-"2.	Describe your idea in detail, including your roles and responsibilities.",
-"3.	Are you collaborating with anyone?  Please list their names.",
-"4.	Is this experience Creativity, Activity, Service, or multiple?  Explain.",
-"5.	Predict which of the 7 learning outcomes you will meet. (see CAS guide)",
-"6.	What, specifically, do you hope to accomplish?",
-"7.	Determine the steps necessary to accomplish your goals and give yourself an approximate deadline.",
-"8.	Determine where you can go for assistance, advice, and information.  (people or resources)",
-"9.	Who will supervise this CAS experience?  (review guidelines if necessary)",
-"10.	Are there any risks involved with this experience?  Explain",
-"11.	How will you document your project?  (journals, spreadsheets, photos, videos, feedback from others, etc.)"];
+new Question("1. Give your CAS activity a descriptive title and date."),
+new Question("2.	Describe your idea in detail, including your roles and responsibilities."),
+new Question("3.	Are you collaborating with anyone?  Please list their names."),
+new Question("4.	Is this experience Creativity, Activity, Service, or multiple?  Explain."),
+new Question("5.	Predict which of the 7 learning outcomes you will meet. (see CAS guide)"),
+new Question("6.	What, specifically, do you hope to accomplish?"),
+new Question("7.	Determine the steps necessary to accomplish your goals and give yourself an approximate deadline."),
+new Question("8.	Determine where you can go for assistance, advice, and information.  (people or resources)"),
+new Question("9.	Who will supervise this CAS experience?  (review guidelines if necessary)"),
+new Question("10.	Are there any risks involved with this experience?  Explain"),
+new Question"11.	How will you document your project?  (journals, spreadsheets, photos, videos, feedback from others, etc.)")];
 
 //making vars for buttons
 var nextb = document.getElementById("btn1");
